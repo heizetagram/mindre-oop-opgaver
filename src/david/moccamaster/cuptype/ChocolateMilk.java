c123ef6f0a70f2693eed5ceabef2aa5290f84aee
David Lu <davidlu1999@gmail.com>
2023-10-05 21:23:46 +0200

    Removed redundant code

--- src/david/moccamaster/cuptype/ChocolateMilk.java
+++ src/david/moccamaster/cuptype/ChocolateMilk.java
@@ -1,15 +1,9 @@
 package david.moccamaster.cuptype;
 
 public class ChocolateMilk extends Cup{
-    private boolean whippedCream;
 
     public ChocolateMilk() {
         super();
         setType("Chocolate Milk");
-        whippedCream = false;
-    }
-
-    public void setWhippedCream(boolean whippedCream) {
-        this.whippedCream = whippedCream;
     }
 }
--- src/david/moccamaster/cuptype/HotCocoa.java
+++ src/david/moccamaster/cuptype/HotCocoa.java
@@ -1,15 +1,9 @@
 package david.moccamaster.cuptype;
 
 public class HotCocoa extends Cup{
-    private boolean whippedCream;
 
     public HotCocoa() {
         super();
         setType("Hot Cocoa");
-        whippedCream = false;
-    }
-
-    public void setWhippedCream(boolean whippedCream) {
-        this.whippedCream = whippedCream;
     }
 }
